4c2890eba48cdf4d47b4d94d7e817fa4fe8d216e
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-25 01:53:06 +0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
-}
+
+  const [inCart, setInCart] = useState(false);
+
+ // Event handler for adding/removing item from cart
+ const handleAddToCart = () => {
+  setInCart((prevInCart) => !prevInCart);
+};
+
+return (
+  <li className={inCart ? "in-cart" : ""}>
+    <span>{name}</span>
+    <span className="category">{category}</span>
+    {/* Button text changes based on inCart state */}
+    <button className="add" onClick={handleAddToCart}>
+      {inCart ? "Remove From Cart" : "Add to Cart"}
+    </button>
+  </li>
+);
+}  
 
 export default Item;
